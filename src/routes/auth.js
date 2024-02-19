1e511fdcec10c74549e259e725c33525d1624e7c
Andre Gomes <andremsgomes@gmail.com>
2024-02-19 19:13:40 +0000

    use multer to send file when signing up

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const multer = require('multer');
+const multerConfig = require('./../config/multer');
+
 const router = express.Router();
 const authController = require("../controllers/auth-ctrl.js");
 
 router.post("/login", authController.login);
-router.post("/signup", authController.signup);
+router.post("/signup", multer(multerConfig).single('avatar'), authController.signup);
 router.put("/update_profile", authController.updateProfile);
 
 module.exports = router;
