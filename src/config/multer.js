a2fc4faad326f8908b49dfde097d68de2c425585
Andre Gomes <andremsgomes@gmail.com>
2024-02-21 22:57:33 +0000

    save images on s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,26 +1,39 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+
+const key = (req, file, cb) => {
+  crypto.randomBytes(16, (err, hash) => {
+    if (err) cb(err);
+
+    const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+    cb(null, fileName);
+  });
+};
+
+const storageTypes = {
+  dev: multerS3({
+    s3: new aws.S3(),
+    bucket: 'histopolio-dev',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key
+  }),
+  prod: multerS3({
+    s3: new aws.S3(),
+    bucket: 'histopolio',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key
+  }),
+}
 
 module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads", "avatars"),
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(
-        null,
-        path.resolve(__dirname, "..", "..", "tmp", "uploads", "avatars")
-      );
-    },
-    filename: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
-
-        const fileName = `${hash.toString('hex')}-${file.originalname}`;
-
-        cb(null, fileName);
-      });
-    },
-  }),
+  storage: storageTypes[process.env.ENV],
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
